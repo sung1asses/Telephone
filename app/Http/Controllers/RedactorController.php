763fec789f12c65d79b851cd336450460ca3b138
sung1asses <suvorov_roman_01@mail.ru>
2020-01-29 16:43:49 +0600

    Redactor controller

--- app/Http/Controllers/RedactorController.php
+++ app/Http/Controllers/RedactorController.php
@@ -11,15 +11,33 @@ class RedactorController extends Controller
         $numbers = Number::get();
         return view('admin.numbers.index', compact('numbers'));
     } 
-    public function createNumbers(){
-name
-position
-cabinet
-email
-local_number
-telephone_number
+    public function createNumbers(Request $request){
+        $validation = \Illuminate\Support\Facades\Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'position' => ['required', 'string', 'max:255'],
+            'cabinet' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'max:255'],
+            'local_number' => ['required', 'string', 'max:255'],
+            'telephone_number' => ['required', 'string', 'max:255'],
+        ]);
+
+        if($validation->fails()) {
+            return redirect()->route('redactor.numbers.list')
+                        ->withInput()
+                        ->with('error', 'Ошибка ввода');
+        };
+        if(!Number::where("name",$request['name'])->first()){
+            Number::create([
+                'name' => $request['name'],
+                'position' => $request['position'],
+                'cabinet' => $request['cabinet'],
+                'email' => $request['email'],
+                'local_number' => $request['local_number'],
+                'telephone_number' => $request['telephone_number'],
+            ]);
+        }
         return redirect()->route('redactor.numbers.list')
-                ->with('succ', 'Очередь успешно создана...');
+                ->with('succ', 'Запись успешно создана!');
     } 
     public function deleteNumbers(){
         return redirect()->route('redactor.numbers.list');
